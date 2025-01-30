941b2b5938ba3c2e03f1f70f0824fcea95832c9a
Vitalii82 <svinovej90@gmail.com>
2025-01-30 15:40:21 +0200

    changed say Hello function

--- lesson2-play-with-git/index.js
+++ lesson2-play-with-git/index.js
@@ -2,7 +2,7 @@ function sayHello(name, surname, someOtherWord) {
     console.log(`Hello ${name} ${surname} ${someOtherWord}`); //second commit
 }
 
-sayHello('Robot', 'Dreams');
+sayHello('Robot', 'Dreams', 'from Vitalii Svinovei' );
 a = 2;
 b = 1;
 console.log(a-b);
