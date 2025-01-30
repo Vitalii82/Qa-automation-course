d1043fc0c422b6d01356cc83092da8ba1605366d
Vitalii82 <svinovej90@gmail.com>
2025-01-30 15:30:34 +0200

    second commit

--- lesson2-play-with-git/index.js
+++ lesson2-play-with-git/index.js
@@ -1,5 +1,8 @@
 function sayHello(name, surname, someOtherWord) {
-    console.log(`Hello ${name} ${surname} ${someOtherWord}`);
+    console.log(`Hello ${name} ${surname} ${someOtherWord}`); //second commit
 }
 
 sayHello('Robot', 'Dreams');
+a = 2;
+b = 1;
+console.log(a-b);
