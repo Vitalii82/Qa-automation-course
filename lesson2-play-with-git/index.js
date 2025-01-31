c7fd90afad8defdaa55dab225b673443367f4744
Vitalii82 <svinovej90@gmail.com>
2025-01-31 18:20:29 +0200

    cleaner up the code 1

--- lesson2-play-with-git/index.js
+++ lesson2-play-with-git/index.js
@@ -1,10 +1,9 @@
 function sayHello(name, surname, someOtherWord) {
-    //some comment
-    console.log(`Hello ${name} ${surname} ${someOtherWord}`); //second commit
+
+    console.log(`Hello ${name} ${surname} ${someOtherWord}`); 
 }
 
 sayHello('Robot', 'Dreams', 'from Vitalii Svinovei' );
 a = 2;
 b = 1;
 console.log(a-b);
-//end function
