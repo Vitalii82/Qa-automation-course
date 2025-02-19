5ef707a38ead334c19db1e635c71a44a96cadb66
Vitalii82 <svinovej90@gmail.com>
2025-02-19 17:25:41 +0200

    arrow function  added

---
+++ lesson7/src/arrow-functions.ts
@@ -0,0 +1,28 @@
+
+const sumArrayArrow = (arr: (number | string)[]): number => {
+    return arr.reduce((sum, element) => {
+      if (typeof element === 'string') {
+        const num = parseFloat(element);
+        if (!isNaN(num)) {
+          return sum + num;
+        }
+      } else if (typeof element === 'number') {
+        return sum + element;
+      }
+      return sum;
+    }, 0);
+  };
+  
+  const stringArray = ['7', '8', '9'];
+  const numberArray = [10, 11, 12];
+  
+  console.log(sumArrayArrow(stringArray)); 
+  console.log(sumArrayArrow(numberArray)); 
+  
+  const mixedArray = [1, '2', '3.5', 4, '5'];
+  const filteredSum = mixedArray
+    .map((element) => parseFloat(element as string)) 
+    .filter((num) => !isNaN(num)) 
+    .reduce((sum, num) => sum + num, 0); 
+  console.log(filteredSum); 
+  
