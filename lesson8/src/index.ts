f61c67d5a059b0e7e1ae3444e36bfcbd52ffd382
Vitalii82 <svinovej90@gmail.com>
2025-02-26 23:33:16 +0200

    eslint fixed and now is work

--- lesson8/src/index.ts
+++ lesson8/src/index.ts
@@ -4,18 +4,18 @@ import { transformUser } from './transformation';
 import { User } from './types';
 import { ExtendedUser } from './abstraction';
 
-async function main() {
-  const users: User[] = await fetchUserData();
-  
- 
-  const shortUserInfos = users.map(user => transformUser(user));
+async function main(): Promise<void> {
+    const users: User[] = await fetchUserData();
 
-  console.log(shortUserInfos);
 
- 
-  const extendedUser = new ExtendedUser(1, 'John Doe', 'johndoe@example.com');
-  console.log(extendedUser.getName());
-  console.log(extendedUser.getEmail());
+    const shortUserInfos = users.map(user => transformUser(user));
+
+    console.log(shortUserInfos);
+
+
+    const extendedUser = new ExtendedUser(1, 'John Doe', 'johndoe@example.com');
+    console.log(extendedUser.getName());
+    console.log(extendedUser.getEmail());
 }
 
 main();
