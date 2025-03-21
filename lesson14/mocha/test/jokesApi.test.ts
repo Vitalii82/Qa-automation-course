48a019871e626f9c7960af9025f7851c0c5b6f1c
Vitalii82 <svinovej90@gmail.com>
2025-03-21 23:03:13 +0200

    lesson 14 has edited

--- lesson14/mocha/test/jokesApi.test.ts
+++ lesson14/mocha/test/jokesApi.test.ts
@@ -1,28 +1,26 @@
 /* eslint-disable unicorn/filename-case */
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import { JokeService } from '../src/services/joke-service';
-
-const jokeService = new JokeService('https://official-joke-api.appspot.com');
+import { JokesApi } from '../src/api/jokes-api';
 
 describe('Jokes Service Tests', () => {
     it('should fetch a random programming joke', async () => {
-        const joke = await jokeService.getRandomProgrammingJoke();
+        const joke = await JokesApi.getRandomProgrammingJoke();
         expect(joke.type).to.equal('programming');
     });
 
     it('should search jokes with a keyword', async () => {
-        const jokes = await jokeService.searchJokeByKeyword('dog');
+        const jokes = await JokesApi.searchJokeByKeyword('dog');
         expect(jokes).to.be.an('array');
     });
 
     it('should fetch the first joke from /ten endpoint', async () => {
-        const joke = await jokeService.getFirstJoke();
+        const joke = await JokesApi.getFirstJoke();
         expect(joke).to.have.property('setup');
     });
 
     it('should fetch the last joke from /ten endpoint', async () => {
-        const joke = await jokeService.getLastJoke();
+        const joke = await JokesApi.getLastJoke();
         expect(joke).to.have.property('punchline');
     });
 });
